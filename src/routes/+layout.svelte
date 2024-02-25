1ea44601f9b0bed5a2e8a205ad080496157da2a9
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-02-25 19:38:12 -0300

    sveltekit store and firebase auth methods done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,63 @@
+<script>
+	import { authStore } from './../store/store.js';
+    import {onMount} from 'svelte';
+    import {auth, db} from '../lib/firebase/firebase';
+    import { getDoc, doc, setDoc } from 'firebase/firestore';
+
+    const nonAuthRoutes = ['/'];
+
+    onMount(() => {
+        console.log('Mounting.')
+        const unsubscribe = auth.onAuthStateChanged(async user => {
+            const currentPath = window.location.pathname;
+
+            if(!user && !nonAuthRoutes.includes(currentPath)) {
+                window.location.href = "/";
+                return
+            }
+
+            if (user && currentPath == "/"){
+                window.location.hred="/dashboard";
+                return
+            }
+
+            if(!user){
+                return;
+            }
+
+            let dataToSetToStore;
+
+            const docRef = doc(db, "users", user.uid)
+            const docSnap = await getDoc(docRef)
+            if(!docSnap.exists()){
+                const userRef = doc(db, 'user', user.uid);
+
+                dataToSetToStore = {
+                    email: user.email, 
+                    todos: [],
+                }
+                await setDoc(
+                    userRef,
+                    dataToSetToStore,
+                    { merge: true }
+                )
+            } else {
+                const userData = docSnap.data();
+                dataToSetToStore = userData;
+            }
+
+            authStore.update((curr) => {
+                return {
+                    ...curr,
+                    user,
+                    data: dataToSetToStore,
+                    loading: false,
+                }
+            })
+        })
+    })
+</script>
+
 <div class="mainContainer">
     <slot />
 </div>
