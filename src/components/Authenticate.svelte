a3e813c4aa401c449e3939d163e23c984d388750
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-02-24 21:57:52 -0300

    first commit

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,31 @@
+<div class="authContainer">
+    <form>
+        <h1>Login</h1>
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+        <h1>Login</h1>
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+        <h1>Login</h1>
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+    </form>
+</div>
+
+<style>
+    .authContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+    }
+</style>
